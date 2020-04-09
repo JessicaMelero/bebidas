054c9c2d96753c9127a5ce46c9a167f7fe36eaf7
jessica <jessicameleromartinez@gmail.com>
2020-04-09 18:43:33 +0200

    Creado formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,7 +1,37 @@
 import React from 'react';
 const Formulario = ({}) => {
   return (
-    <h1>Formulario</h1>
+    <form className="col-12 mt-4">
+      <fieldset className="text-center">
+        <legend>Busca bebidas por categoría o ingrediente</legend>
+      </fieldset>
+      <div className="row">
+        <div className="col-md-4">
+          <input
+            name="nombre"
+            className="form-control"
+            type="text"
+            placeholder="Buscar por ingrediente"
+          />
+        </div>
+        <div className="col-md-4">
+          <select
+            className="form-control"
+            name="categoria"
+          >
+            <option value="">-- Selecciona una categoría --</option>
+          </select>
+        </div>
+
+        <div className="col-md-4">
+          <input
+            type="submit"
+            className="btn btn-block btn-primary"
+            value="Buscar bebidas"
+          />
+        </div>
+      </div>
+    </form>
   );
 }
 
