a8b1ed112217bf1aa45787f7bd138882484ea7bc
jessica <jessicameleromartinez@gmail.com>
2020-04-10 21:52:57 +0200

    Updates

---
+++ precache-manifest.ec6853de586244c0d04ab05112873dd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cfa2329c60dbf36551ebee34d9a5b3a",
+    "url": "/bebidas/index.html"
+  },
+  {
+    "revision": "10aac30deaf283ec91e6",
+    "url": "/bebidas/static/css/main.7e603ed8.chunk.css"
+  },
+  {
+    "revision": "55c41921d66c282c06ba",
+    "url": "/bebidas/static/js/2.85d521af.chunk.js"
+  },
+  {
+    "revision": "58aabe32aae325c8d2caf2ff6956fc48",
+    "url": "/bebidas/static/js/2.85d521af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10aac30deaf283ec91e6",
+    "url": "/bebidas/static/js/main.c8b8bfdb.chunk.js"
+  },
+  {
+    "revision": "4dd81a835d204362160b",
+    "url": "/bebidas/static/js/runtime-main.d783216a.js"
+  }
+]);
