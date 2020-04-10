4a03f82bcc787d9b779abb9d39c6b86b56f0bb16
jessica <jessicameleromartinez@gmail.com>
2020-04-10 19:10:03 +0200

    Creado context Modal

---
+++ src/context/ModalContext.js
@@ -0,0 +1,22 @@
+import React, {createContext, useState, useEffect} from 'react';
+import axios from 'axios';
+
+// crear el context
+export const ModalContext = createContext();
+
+const ModalProvider = (props) => {
+
+  // state del Provider
+  const [ idreceta, guardarIdReceta ] = useState(null);
+
+  return (
+    <ModalContext.Provider
+      value={{
+        guardarIdReceta
+      }}
+    >{props.children}
+    </ModalContext.Provider>
+  );
+}
+
+export default ModalProvider;
